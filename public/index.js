d78c068333fe96e28932f33ecf4c0a66019215e5
Sebastian Johansson <sebben.97@hotmail.com>
2019-12-06 14:17:09 +0100

    small changes

--- public/index.js
+++ public/index.js
@@ -2,23 +2,23 @@ var socket = io()
 const messageList = document.getElementById('messages')
 const typing = document.getElementById('typing')
 const messageInput = document.getElementById('message')
-const commands = document.getElementById("commands")
-const messageForm = document.getElementById("messageForm")
-const userForm = document.getElementById('userForm');
+const commands = document.getElementById('commands')
+const messageForm = document.getElementById('messageForm')
+const userForm = document.getElementById('userForm')
 const usernameInput = document.getElementById('username')
 let username = $('#username')
 //Shows message
 socket.on('message', function(msg){
-    let li = document.createElement('li');
+    let li = document.createElement('li')
     li.innerHTML = msg
     messageList.append(li)
     scrollBottom()
 });
 //Show gif
-socket.on("recieve gif", function(data, username){
-    const li = document.createElement("li")
-    const image = document.createElement("img")
-    image.className = "rounded mx-auto d-block mt-1"
+socket.on('recieve gif', function(data, username){
+    const li = document.createElement('li')
+    const image = document.createElement('img')
+    image.className = 'rounded mx-auto d-block mt-1'
     li.innerHTML = '<span style="font-size:13px;font-weight:700;text-decoration:underline;">' + username + ':</span><br>'
     image.src = JSON.parse(data).data[0].images.downsized.url
     li.append(image)
@@ -27,63 +27,60 @@ socket.on("recieve gif", function(data, username){
 })
 
 function showCommands(){
-    if(messageInput.value === "/"){
-        commands.style.display = "flex"
+    if(messageInput.value === '/'){
+        commands.style.display = 'flex'
     }else {
-        commands.style.display = "none"
+        commands.style.display = 'none'
     }
 }
 
-$('#userForm').on("submit", function(e){
+$('#userForm').on('submit', function(e){
     let string = username.val()
     let upperCaseUsername = string[0].toUpperCase() + string.slice(1)
-    if(username.val() === ""){
-        alert("You have to choose a username!")
+    if(username.val() === ''){
+        alert('You have to choose a username!')
     }else{
         socket.emit('set user', upperCaseUsername, function(data){
-            $('#userFormWrap').hide();
-            $('#mainWrap').show();
+            $('#userFormWrap').hide()
+            $('#mainWrap').show()
         });
 
     }
-    e.preventDefault();
+    e.preventDefault()
 })
 
 function scrollBottom() {
-    var log = $('#messageContainer');
-    log.scrollTop(log.prop("scrollHeight"));
+    var log = $('#messageContainer')
+    log.scrollTop(log.prop('scrollHeight'))
 }
 
 socket.on('typing', function(data){
-    typing.innerHTML = '<p><em>' + data.user + ' is typing...</em></p>'; 
+    typing.innerHTML = '<p><em>' + data.user + ' is typing...</em></p>'
 })
 socket.on('not typing', function(){
-    typing.innerHTML = ""   
+    typing.innerHTML = ''   
 })
 function initSite(){
     messageInput.addEventListener('keyup', function(e){
-        if(e.keyCode === 13 || messageInput.value === ""){
-            socket.emit("not typing")
+        if(e.keyCode === 13 || messageInput.value === ''){
+            socket.emit('not typing')
         }else{
             socket.emit('typing')
         }
     })
     
-    messageForm.addEventListener("submit", function(e){
+    messageForm.addEventListener('submit', function(e){
         e.preventDefault();
-        if(messageInput.value.indexOf("/") === 0){
-            if(messageInput.value.indexOf("/gif") === 0){
+        if(messageInput.value.indexOf('/') === 0){
+            if(messageInput.value.indexOf('/gif') === 0){
                 socket.emit("gif", messageInput.value.substring(5))
-            }
-            if(messageInput.value.indexOf("/leave") === 0){
-                          
             }
         }else {
             if(messageInput.value){
-                socket.emit('chat message', messageInput.value);
+                socket.emit('chat message', messageInput.value)
             }
         }
-        messageInput.value = "";
+        messageInput.value = ''
         socket.emit("not typing")
     })
 }
--- server.js
+++ server.js
@@ -1,45 +1,42 @@
 const express = require('express')
 const app = express()
 const request = require('request')
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-let key = "P3AQOb2xS2adhA2lOUe4kgRmuuGn0MMp"
-var users = [];
+var http = require('http').createServer(app)
+var io = require('socket.io')(http)
+let key = 'P3AQOb2xS2adhA2lOUe4kgRmuuGn0MMp'
+var users = []
 
 app.use(express.static('public'))
 
 io.on('connection', function(socket){
     socket.on('chat message', function(msg){
         io.send('<span style="font-size:13px;font-weight:700;text-decoration:underline;">' + socket.username + '</span>' + '<br>' + msg)
-    });
+    })
     socket.on('disconnect', function(data){
-        socket.broadcast.emit("not typing")
+        socket.broadcast.emit('not typing')
     })
     socket.on('typing', function(data){
         socket.broadcast.emit('typing', {
             msg: data,
             user: socket.username
-        });
+        })
     })
     socket.on('not typing', function(){
         socket.broadcast.emit('not typing')
     })
- 
     socket.on('set user' , (data, callback) =>{
         if(data === ''){
-            callback(false);
+            callback(false)
         }else{
-            callback(true);
-            socket.username = data;
-            users.push(socket.username);
-            updateUsers();
+            callback(true)
+            socket.username = data
+            users.push(socket.username)
+            updateUsers()
         }
     })
-
     function updateUsers(){
        io.sockets.emit('users', users); 
     }
-   
     socket.on('gif', function(topic){
         request(`http://api.giphy.com/v1/gifs/search?q=${topic}&api_key=${key}&limit=5`, function(err, response, body){
             if(err){
@@ -49,10 +46,9 @@ io.on('connection', function(socket){
             }   
         })
     })
-
-});
+})
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+  console.log('listening on *:3000')
+})
 
